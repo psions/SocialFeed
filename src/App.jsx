cc6f60b9becfd4894f2f4a4893710c7ae3321260
psions <psionreality@gmail.com>
2022-04-04 13:41:55 -0500

    Created multiple jsx files + createpost,displaypost,navbar and post all jsx files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NamePresenter from "./components/NamePresenter";
+
+function App() {
+  return (
+    <div >
+      Hello World!
+      <NamePresenter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NamePresenter.jsx
@@ -0,0 +1,9 @@
+import React from "react";     
+
+const NamePresenter = (props) => {
+    return <div >I'm From the name component</div>;
+};
+    
+    
+ 
+export default NamePresenter;
